69cee20aa9d892bff7bd5dd868275ff58f06eeaa
annpryshchepa <pryshchepa.hanna@gmail.com>
2020-11-16 00:05:50 +0200

    Completed task with various random options

---
+++ task7.js
@@ -0,0 +1,6 @@
+// Generate random decimal number in range 0 to 1;
+
+console.log(Math.random());
+console.log(Math.random().toFixed(2)); // with 2 numbers after coma
+console.log(Math.random() * (4 - 3) + 3); // random numbers between 3 and 4
+console.log((Math.random() * (5 - 0) + 0).toFixed(3)) // random numbers between 0 and 5 with 3 digits after coma
